3e98c9f214211812688bdad465e7f1597f45a2f9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-07 19:02:10 -0600

    Initial Commit

---
+++ MetaIntroToEnums/MetaIntroToEnumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaIntroToEnumsApp.swift
+//  MetaIntroToEnums
+//
+//  Created by Mitch Andrade on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaIntroToEnumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
